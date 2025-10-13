50495441b50fc933e3f05e1145b84accd7c25e2a
Siyam Mia <siyambd188@gmail.com>
2025-10-13 20:41:15 +0600

    Create even_odd_counter.c

---
+++ even_odd_counter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);  
+    int num;
+    int even = 0, odd = 0, positive = 0, negative = 0;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        if(num % 2 == 0)
+            even++;
+        else
+            odd++;
+
+        if(num > 0)
+            positive++;
+        else if(num < 0)
+            negative++;
+    }
+
+    printf("Even: %d\n", even);
+    printf("Odd: %d\n", odd);
+    printf("Positive: %d\n", positive);
+    printf("Negative: %d\n", negative);
+
+    return 0;
+}
