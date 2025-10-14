bd64651d85557695a37bc9eb529627abc8763c66
Siyam Mia <siyambd188@gmail.com>
2025-10-15 00:03:31 +0600

    Create count_odd_numbers.c

---
+++ count_odd_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);    
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+           count++;
+        }
+    }
+    printf("%d",count);
+    
+    return 0;
+}
