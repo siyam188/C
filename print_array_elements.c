e15b7d230303fcb36438bd7de0f9e71e891846d9
Siyam Mia <siyambd188@gmail.com>
2025-10-14 22:46:09 +0600

    Create print_array_elements.c

---
+++ print_array_elements.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int arr[7];
+    arr[0]=1;
+    arr[1]=2;
+    arr[2]=3;
+    arr[3]=4;
+    arr[4]=5;
+    arr[5]=6;
+    arr[6]=7;
+    printf("%d\n",arr[0]);
+    printf("%d\n",arr[1]);
+    printf("%d\n",arr[2]);
+    printf("%d\n",arr[3]);
+    printf("%d\n",arr[4]);
+    printf("%d\n",arr[5]);
+    printf("%d\n",arr[6]);
+
+    return 0;
+}
