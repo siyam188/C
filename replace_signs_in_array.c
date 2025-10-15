cd2d81cddb6713bef540020f7f91ded46b714254
Siyam Mia <siyambd188@gmail.com>
2025-10-16 00:55:56 +0600

    Create replace_signs_in_array.c

---
+++ replace_signs_in_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N); 
+
+    int A[1000];   
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (A[i] > 0)
+            A[i] = 1;
+        else if (A[i] < 0)
+            A[i] = 2;
+    }
+
+    for (int i = 0; i < N; i++) {
+        printf("%d ", A[i]);
+    }
+
+    return 0;
+}
