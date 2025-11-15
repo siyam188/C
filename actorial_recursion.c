44dbaa24280526a8b2ad5034c910a28a2c5fd19c
Siyam Mia <siyambd188@gmail.com>
2025-11-15 21:33:39 +0600

    Create actorial_recursion.c

---
+++ actorial_recursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+long long int fact(int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+  long long int res =  fact(n-1);
+      return res*n;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+  long long int ans= fact(n);
+   printf("%lld",ans);
+    return 0;
+}
