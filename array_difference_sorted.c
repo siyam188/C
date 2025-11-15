afb1ede01be6df8690a454c72386c05449b363e1
Siyam Mia <siyambd188@gmail.com>
2025-11-15 21:34:50 +0600

    Add array_difference_sorted.c for calculating differences
    Implement a program to calculate the absolute difference between two sorted arrays.

---
+++ array_difference_sorted.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    while (x--)
+    {
+    
+    int n;
+    scanf("%d",&n);
+    int a[n],b[n],c[n];
+    for(int i =0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        b[i]=a[i];
+    }
+     for(int i =0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(b[i]>b[j])
+            {
+            int temp = b[i];
+            b[i]= b[j];
+            b[j]=temp;
+            }
+        }
+     
+    }
+    
+     for(int i =0;i<n;i++)
+    {
+      c[i] =abs(a[i]-b[i]);
+    }
+    for(int i =0;i<n;i++)
+    {
+      printf("%d ",c[i]);
+    }
+    printf("\n");
+}
+
+    
+    return 0;
+}
